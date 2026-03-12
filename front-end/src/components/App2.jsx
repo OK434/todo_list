ac042c10e09ee6d0dc0a66838abdab9027304235
Omar Rashed <o.mar.om.123m@gmail.com>
2026-03-12 23:53:36 +0300

    update app2

--- front-end/src/components/App2.jsx
+++ front-end/src/components/App2.jsx
@@ -9,60 +9,12 @@ export default function App2() {
     projects: [],
     tasks: [],
   });
-  function handleAddTask(text) {
-    setSelectPro((prevState) => {
-      const taskId = Math.random().toString();
-      const newtask = {
-        text: text,
-        projectId: prevState.selectedProjectId,
-        id: taskId,
-      };
-
-      return {
-        ...prevState,
-        tasks: [...prevState.tasks, newtask],
-      };
-    });
-  }
-  function handleDelTask(id) {
-    setSelectPro((prevState) => ({
-      ...prevState,
-
-      tasks: selectPro.tasks.filter((task) => task.id !== id),
-    }));
-  }
-
   function handleStartAddPro() {
     setSelectPro((prevState) => ({
       ...prevState,
       selectedProjectId: null,
     }));
   }
-
-
-  function deleteTask() {
-    setSelectPro((prevState) => ({
-      ...prevState,
-      selectedProjectId: undefined,
-      projects: selectPro.projects.filter(
-        (project) => project.id !== selectPro.selectedProjectId
-      ),
-    }));
-  }
-  const selectedProject = selectPro.projects.find(
-    (project) => project.id === selectPro.selectedProjectId
-  );
-  let content = (
-    <SelectPro
-      project={selectedProject}
-      onDelete={deleteTask}
-      onAddTask={handleAddTask}
-      onDelTask={handleDelTask}
-      tasks={selectPro.tasks}
-    />
-  );
-
-
   return (
     <main className="h-screen my-8 flex gap-8">
       {<Calendar onStartAddPro={handleStartAddPro} />}
