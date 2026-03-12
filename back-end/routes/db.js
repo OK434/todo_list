b39e237861cf690bf6990183b3f98adfd83d4c78
Omar Rashed <o.mar.om.123m@gmail.com>
2026-03-12 21:32:57 +0300

    del file

--- back-end/routes/db.js
+++ back-end/routes/db.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-
-
-mongoose.connect("mongodb://127.0.0.1:27017/todo")
-.then(() => console.log("DB connected"))
-.catch(err => console.log("DB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB connected");
+})
+.catch((err) => {
+  console.error("DB connection error:", err);
+});
